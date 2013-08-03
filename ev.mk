5191ce0952b76077b541e111af431513aea19c98
kristofpetho <kristof.petho@gmail.com>
2013-08-03 09:26:35 +0200

    m7: update build description, id, fingerprint
    Change-Id: Ieaec8f271e2c26ccbb7b0efca8d637be0e16e7db

--- ev.mk
+++ ev.mk
@@ -15,7 +15,7 @@ PRODUCT_MODEL := One
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_FINGERPRINT="htc_europe/m7/m7:4.1.2/JZO54K/177101.12:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.12 CL177101 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Hostis
